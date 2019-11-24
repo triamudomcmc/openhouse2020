2b16fe33243f405920918d5061973dc802d5416f
iammarkps <markkung08@gmail.com>
2019-11-24 11:44:18 +0700

    :lipstick: Add diag BG

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,21 @@
 import styled from '@emotion/styled'
 
-export const Card = styled.div<{ width: number; height: number; bg?: string }>`
-  border-radius: 8px;
+export const Card = styled.div<{
+  width: number
+  height: number
+  bg?: string
+  bordered?: boolean
+}>`
+  ${props =>
+    props.bordered
+      ? `border-radius: 8px;
+  border-color: #323639;
+  border-style: solid;
+  border-width: 0.5px;`
+      : ''}
+
   box-shadow: 0 5px 10px rgba(0, 0, 0, 0.12);
   color: white;
-  border: grey;
   background-color: ${props => (props.bg ? props.bg : '#111111')};
   ${props => `width: ${props.width}px`};
   ${props => `height: ${props.height}px`};
--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -4,13 +4,20 @@ import { Card } from './Card'
 
 const Wrapper = styled.div`
   max-width: 100%;
-  display: flex;
-  align-items: center;
+  height: 70vh;
+  color: white;
+  text-align: center;
   background-color: black;
   overflow-y: hidden;
   padding-top: 24px;
   padding-bottom: 24px;
+`
+
+const FeaturedSliderWrapper = styled.div`
+  display: flex;
+  align-items: center;
   position: relative;
+  margin-top: 48px;
 `
 
 const FeaturedSlider = styled.div`
@@ -37,44 +44,63 @@ const FeaturedSlider = styled.div`
   }
 `
 
+const DiagTop = styled.div`
+  border-style: solid;
+  border-width: 50px 100vw 0 0;
+  border-color: transparent black transparent transparent;
+`
+
+const DiagBottom = styled.div`
+  border-style: solid;
+  border-width: 50px 100vw 0 0;
+  border-color: black transparent transparent transparent;
+`
+
 export const Featured = () => {
   return (
-    <Wrapper>
-      <FeaturedSlider>
-        <Card width={280} height={254}>
-          <div>Hellooooooo</div>
-        </Card>
-        <Card width={280} height={254}>
-          <div>Hellooooooo</div>
-        </Card>
-        <Card width={280} height={254}>
-          <div>Hellooooooo</div>
-        </Card>
-        <Card width={280} height={254}>
-          <div>Hellooooooo</div>
-        </Card>
-        <Card width={280} height={254}>
-          <div>Hellooooooo</div>
-        </Card>
-        <Card width={280} height={254}>
-          <div>Hellooooooo</div>
-        </Card>
-        <Card width={280} height={254}>
-          <div>Hellooooooo</div>
-        </Card>
-        <Card width={280} height={254}>
-          <div>Hellooooooo</div>
-        </Card>
-        <Card width={280} height={254}>
-          <div>Hellooooooo</div>
-        </Card>
-        <Card width={280} height={254}>
-          <div>Hellooooooo</div>
-        </Card>
-        <Card width={280} height={254}>
-          <div>Hellooooooo</div>
-        </Card>
-      </FeaturedSlider>
-    </Wrapper>
+    <section>
+      <DiagTop />
+      <Wrapper>
+        <h1>Featured</h1>
+        <FeaturedSliderWrapper>
+          <FeaturedSlider>
+            <Card width={280} height={254}>
+              <div>Hellooooooo</div>
+            </Card>
+            <Card width={280} height={254}>
+              <div>Hellooooooo</div>
+            </Card>
+            <Card width={280} height={254}>
+              <div>Hellooooooo</div>
+            </Card>
+            <Card width={280} height={254}>
+              <div>Hellooooooo</div>
+            </Card>
+            <Card width={280} height={254}>
+              <div>Hellooooooo</div>
+            </Card>
+            <Card width={280} height={254}>
+              <div>Hellooooooo</div>
+            </Card>
+            <Card width={280} height={254}>
+              <div>Hellooooooo</div>
+            </Card>
+            <Card width={280} height={254}>
+              <div>Hellooooooo</div>
+            </Card>
+            <Card width={280} height={254}>
+              <div>Hellooooooo</div>
+            </Card>
+            <Card width={280} height={254}>
+              <div>Hellooooooo</div>
+            </Card>
+            <Card width={280} height={254}>
+              <div>Hellooooooo</div>
+            </Card>
+          </FeaturedSlider>
+        </FeaturedSliderWrapper>
+      </Wrapper>
+      <DiagBottom />
+    </section>
   )
 }
