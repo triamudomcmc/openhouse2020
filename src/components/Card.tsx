0dbd4bf0f3fb354380e676a487acc95ee03c99f9
iammarkps <markkung08@gmail.com>
2019-11-25 23:01:14 +0700

    :sparkles: Add featured card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,6 +3,7 @@ import styled from '@emotion/styled'
 export const Card = styled.div<{
   width: number
   height: number
+  flex?: boolean
   bg?: string
   bordered?: boolean
 }>`
@@ -17,6 +18,48 @@ export const Card = styled.div<{
   box-shadow: 0 5px 10px rgba(0, 0, 0, 0.12);
   color: white;
   background-color: ${props => (props.bg ? props.bg : '#111111')};
+  ${props => (props.flex ? `display: flex; flex-direction: column;` : '')}
   ${props => `width: ${props.width}px`};
   ${props => `height: ${props.height}px`};
 `
+
+const FeaturedCardImage = styled.figure`
+  height: 120px;
+  margin: 0;
+  overflow: hidden;
+
+  img {
+    object-fit: cover;
+    width: 100%;
+  }
+`
+
+const FeaturedCardTitle = styled.div`
+  padding: 10px 20px 15px;
+  color: #fff;
+  text-align: left;
+`
+
+export const FeaturedCard = (props: {
+  width: number
+  height: number
+  imgSrc: string
+  title: string
+  bg?: string
+  bordered?: boolean
+}) => (
+  <Card
+    flex
+    width={props.width}
+    height={props.height}
+    bg={props.bg}
+    bordered={props.bordered}
+  >
+    <FeaturedCardImage>
+      <img decoding="async" src={props.imgSrc}></img>
+    </FeaturedCardImage>
+    <FeaturedCardTitle>
+      <h1>{props.title}</h1>
+    </FeaturedCardTitle>
+  </Card>
+)
