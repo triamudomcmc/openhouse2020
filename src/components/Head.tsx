c4894ec9904ae7bcf114a59c19d3f0ccc8c88108
iammarkps <markkung08@gmail.com>
2019-12-05 21:12:42 +0700

    :mag: Add head

---
+++ src/components/Head.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Head from 'next/head'
+
+export const CustomHead = () => (
+  <Head>
+    <meta property="og:url" content="https://openhouse.triamudom.ac.th/" />
+    <meta property="og:title" content="Triam Udom Open House 2020" />
+    <meta
+      property="og:description"
+      content="นิทรรศการวิชาการ โรงเรียนเตรียมอุดมศึกษา ประจำปี ๒๕๖๒"
+    />
+    <meta
+      property="description"
+      content="นิทรรศการวิชาการ โรงเรียนเตรียมอุดมศึกษา ประจำปี ๒๕๖๒"
+    />
+    <meta
+      property="og:image"
+      content="https://openhouse.triamudom.ac.th/assets/img/og.png"
+    />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@triamudomoph" />
+    <meta property="og:site_name" content="Triam Udom Open House" />
+    <meta name="twitter:image:alt" content="Triam Udom Open House 2020" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+    />
+    <title>Triam Udom Open House 2020</title>
+  </Head>
+)
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
-import React from "react"
-import App from "next/app"
-import { GlobalStyle, theme } from "../design"
-import { ThemeProvider } from "emotion-theming"
+import React from 'react'
+import App from 'next/app'
+import { ThemeProvider } from 'emotion-theming'
+
+import { GlobalStyle, theme } from '../design'
+import { CustomHead } from '../components/Head'
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
+
     return (
       <ThemeProvider theme={theme}>
+        <CustomHead />
         <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
