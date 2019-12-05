5631ba8892dcfe869281ee537db93c188a50f3a0
iammarkps <markkung08@gmail.com>
2019-12-05 21:14:01 +0700

    :sparkles: Add inside layout

---
+++ src/components/Inside.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const Wrapper = styled.div`
+  max-width: 100%;
+  height: 100vh;
+  color: white;
+  text-align: center;
+  background-color: #1c1e28;
+  padding-top: 24px;
+  padding-bottom: 24px;
+`
+
+export const Inside = () => <Wrapper></Wrapper>
